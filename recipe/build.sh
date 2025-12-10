3bbdeea355621978c78a3abfae7a15eb3059adbf
mementorc <claude.rc@gmail.com>
2025-12-10 15:45:44 -0600

    (fix) no test

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 # from https://github.com/Homebrew/homebrew-core/blob/master/Formula/opam.rb#L24-L27
 
-./configure --prefix=$PREFIX --with-vendored-deps && make && make test && make install
+./configure --prefix=$PREFIX --with-vendored-deps && make && make install
 
 # if [[ "${target_platform}" == "linux-"* ]] || [[ "${target_platform}" == "osx-64" ]]; then
 #   make libinstall
